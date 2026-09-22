125d2d688786887181ab28e6f6f9f44ef3f4a549
Rafael <heyrafiz.1@gmail.com>
2026-09-22 20:06:27 -0300

    prototipo

---
+++ src/main/java/com/mycompany/academia/Academia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.academia;
+
+/**
+ *
+ * @author aluno.saolucas
+ */
+public class Academia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
